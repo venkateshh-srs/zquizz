add13095cc85b49aac96f804712161578a7813e8
venkateshh <venky@gmail.com>
2024-09-10 21:17:47 +0530

    a

--- backend/app.js
+++ backend/app.js
@@ -13,32 +13,23 @@ mongoose.connection.on("connected", () => {
   console.log("connected to mongodb atlas");
 });
 const app = express();
-const port = 3000;
-app.use(cookieParser());
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+const port = 3000;
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("hello world...");
 });
-// app.get("/quiz/:id", async (req, res) => {
-//   //if admin show preview
-//   const quizId = req.params.id;
-//   const quiz = await Quizes.findOne({ quizId });
-//   if (!quiz) {
-//     return res.status(404).json({ message: "Quiz not found" });
-//   }
-//   const questions = quiz.questions;
-//   res.status(200).json({ message: questions });
-// });
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
